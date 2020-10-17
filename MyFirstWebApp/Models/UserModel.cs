7265abce60277d0c5c45bc9217f01f9d2a13f7bb
ravinder samudrala <ravinder.samudrala@gmail.com>
2020-10-17 23:01:08 +0530

    Login and registration started working

---
+++ MyFirstWebApp/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstWebApp.Models
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+        [Required(AllowEmptyStrings =false, ErrorMessage ="First Name is Required")]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "last Name is Required")]
+        public string LastName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is Required")]
+        public string Email { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "PassWord is Required")]
+        public string PassWord { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Confirm Password is Required")]
+        [DataType(DataType.Password)]
+        [Compare("PassWord", ErrorMessage ="Password and confirm Password should match")]
+        public string ConfirmPassword { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
